273f0ab871277d01133f0c0392b9a7e496586247
Adam Tse <mr.adam.tse@gmail.com>
2016-11-27 21:03:54 -0500

    added bootstrap / joke logic

--- src/app/joke/joke.component.ts
+++ src/app/joke/joke.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Input, Output } from '@angular/core';
+import { Joke } from '../joke';
 
 @Component({
-  selector: 'app-joke',
+  selector: 'joke',
   templateUrl: './joke.component.html',
   styleUrls: ['./joke.component.css']
 })
 export class JokeComponent implements OnInit {
 
   constructor() { }
+
+  @Input('joke') data: Joke;
+  @Output() jokeDeleted = new EventEmitter<Joke>();
+
+  deleteItem() {
+    this.jokeDeleted.emit(this.data);
+  }
 
   ngOnInit() {
   }
